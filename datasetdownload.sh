080422dcfd6fbce020881d58013984514a1bbc4b
Pedro <pe25171@mit.edu>
2018-11-06 08:04:16 -0500

    Dataset downloader

---
+++ datasetdownload.sh
@@ -0,0 +1,4 @@
+mkdir retrogan
+cd retrogan
+wget https://conceptnet.s3.amazonaws.com/downloads/2017/numberbatch/numberbatch-en-17.06.txt.gz
+wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M-subword.vec.zip
