dbe0aebd81c704625b4b3a219529c89daf55361c
Pedro <pe25171@mit.edu>
2019-01-21 20:57:01 -0500

    Testing!

--- gantests3.py
+++ gantests3.py
@@ -32,7 +32,7 @@ class RetroCycleGAN():
         # self.disc_patch = (patch, patch, 1)
 
         # Number of filters in the first layer of G and D
-        self.gf = 128
+        self.gf = 256
         self.df = 512
 
         # Loss weights
