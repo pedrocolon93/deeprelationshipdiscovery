19d46e42b2af39612d9467d5b36acc8a6aa0163c
Pedro <pe25171@mit.edu>
2019-04-24 16:25:23 -0400

    checking for errors

--- deep_relationship_discovery.py
+++ deep_relationship_discovery.py
@@ -11,26 +11,28 @@ import sklearn
 from conceptnet5.vectors import standardized_concept_uri
 from keras import Input, Model
 from keras.engine.saving import load_model
-from keras.layers import Dense, Concatenate, BatchNormalization, Conv1D, Multiply, multiply
+from keras.layers import Dense, Concatenate, BatchNormalization, Conv1D, multiply
 from keras.optimizers import Adam
 # from keras.utils import plot_model
 from tqdm import tqdm
 
 from retrogan_trainer import attention, ConstMultiplierLayer
 
-relations = ["/r/PartOf","/r/IsA", "/r/HasA", "/r/UsedFor", "/r/CapableOf", "/r/Desires",
+relations = ["/r/PartOf", "/r/IsA", "/r/HasA", "/r/UsedFor", "/r/CapableOf", "/r/Desires",
              "/r/AtLocation",
              "/r/Causes", "/r/HasSubevent", "/r/HasFirstSubevent", "/r/HasLastSubevent", "/r/HasPrerequisite",
              "/r/HasProperty", "/r/MotivatedByGoal", "/r/ObstructedBy", "/r/CreatedBy", "/r/Synonym",
              "/r/Antonym", "/r/DistinctFrom", "/r/DerivedFrom", "/r/SymbolOf", "/r/DefinedAs", "/r/Entails",
-             "/r/MannerOf","/r/RelatedTo",
-             "/r/LocatedNear", "/r/HasContext", "/r/FormOf","/r/SimilarTo", "/r/EtymologicallyRelatedTo",
+             "/r/MannerOf", "/r/RelatedTo",
+             "/r/LocatedNear", "/r/HasContext", "/r/FormOf", "/r/SimilarTo", "/r/EtymologicallyRelatedTo",
              "/r/EtymologicallyDerivedFrom", "/r/CausesDesire", "/r/MadeOf", "/r/ReceivesAction", "/r/InstanceOf",
              "/r/NotDesires", "/r/NotUsedFor", "/r/NotCapableOf", "/r/NotHasProperty"]
+
 
 def conv1d(layer_input, filters, f_size=6, strides=1, normalization=True):
     d = Conv1D(filters, f_size, strides=strides, activation="relu")(layer_input)
     return d
+
 
 def create_model():
     # Input needs to be 2 word vectors
@@ -39,7 +41,7 @@ def create_model():
     expansion_size = 512
     filters = 8
     # Expand and contract the 2 word vectors
-    wv1_expansion_1 = Dense(expansion_size*2)(wv1)
+    wv1_expansion_1 = Dense(expansion_size * 2)(wv1)
     wv1_expansion_1 = BatchNormalization()(wv1_expansion_1)
     # r_1 = Reshape((-1, 1))(wv1_expansion_1)
     # t1 = conv1d(r_1, filters, f_size=4)
@@ -47,10 +49,10 @@ def create_model():
     # f1 = Flatten()(f1)
     # wv1_expansion_2 = attention(f1)
     wv1_expansion_2 = attention(wv1_expansion_1)
-    wv1_expansion_3 = Dense(int(expansion_size / 4),activation='relu')(wv1_expansion_2)
+    wv1_expansion_3 = Dense(int(expansion_size / 4), activation='relu')(wv1_expansion_2)
     wv1_expansion_3 = BatchNormalization()(wv1_expansion_3)
 
-    wv2_expansion_1 = Dense(expansion_size*2)(wv2)
+    wv2_expansion_1 = Dense(expansion_size * 2)(wv2)
     wv2_expansion_1 = BatchNormalization()(wv2_expansion_1)
     # r_2 = Reshape((-1, 1))(wv2_expansion_1)
     # t2 = conv1d(r_2, filters, f_size=4)
@@ -60,20 +62,20 @@ def create_model():
     # wv2_expansion_2 = Dense(int(expansion_size / 2),activation='relu')(wv2_expansion_1)
     # wv2_expansion_2 = BatchNormalization()(wv2_expansion_1)
     wv2_expansion_2 = attention(wv2_expansion_1)
-    wv2_expansion_3 = Dense(int(expansion_size / 4),activation='relu')(wv2_expansion_2)
+    wv2_expansion_3 = Dense(int(expansion_size / 4), activation='relu')(wv2_expansion_2)
     wv2_expansion_3 = BatchNormalization()(wv2_expansion_3)
 
     # Concatenate both expansions
     merge1 = Concatenate()([wv1_expansion_3, wv2_expansion_3])
-    merge_expand = Dense(expansion_size,activation='relu')(merge1)
+    merge_expand = Dense(expansion_size, activation='relu')(merge1)
     merge_expand = BatchNormalization()(merge_expand)
     # Add atention layer
     merge_attention = attention(merge_expand)
-    attention_expand = Dense(expansion_size,activation='relu')(merge_attention)
+    attention_expand = Dense(expansion_size, activation='relu')(merge_attention)
     attention_expand = BatchNormalization()(attention_expand)
-    semi_final_layer = Dense(expansion_size,activation='relu')(attention_expand)
+    semi_final_layer = Dense(expansion_size, activation='relu')(attention_expand)
     semi_final_layer = BatchNormalization()(semi_final_layer)
-    common_layers_model = Model([wv1, wv2],semi_final_layer,name="Common layers")
+    common_layers_model = Model([wv1, wv2], semi_final_layer, name="Common layers")
     # common_optimizer = Adam(lr=0.000002)
     # common_layers_model.compile()
     # Output layer
@@ -85,46 +87,44 @@ def create_model():
     losses = []
     model_dict = {}
     prob_model_dict = {}
-    #FOR PICS
+    # FOR PICS
     model_outs = []
     for rel in relations:
-        task_layer = Dense(task_layer_neurons,activation='relu')(semi_final_layer)
+        task_layer = Dense(task_layer_neurons, activation='relu')(semi_final_layer)
         task_layer = BatchNormalization()(task_layer)
         task_layer = attention(task_layer)
-        task_layer = Dense(task_layer_neurons,activation='relu')(task_layer)
+        task_layer = Dense(task_layer_neurons, activation='relu')(task_layer)
         task_layer = BatchNormalization()(task_layer)
-
 
         layer_name = rel.replace("/r/", "")
         loss = "mean_squared_error"
         losses.append(loss)
 
         # out = Dense(1,name=layer_name)(task_layer)
-        probability = Dense(units=1,activation='sigmoid')(task_layer)
-        scale = ConstMultiplierLayer()(task_layer)
-        out = multiply([scale,probability])
+        probability = Dense(units=1, activation='sigmoid',name=layer_name+"_prob")(task_layer)
+        # out = multiply([scale, probability])
+        scale = ConstMultiplierLayer()(probability)
 
-        model_outs.append(out)
-        drdp = Model([wv1,wv2],probability,name=layer_name+"probability")
-        drd = Model([wv1, wv2], out,name=layer_name)
+        model_outs.append(scale)
+        drdp = Model([wv1, wv2], probability, name=layer_name + "probability")
+        drd = Model([wv1, wv2], scale, name=layer_name)
         optimizer = Adam(lr=0.0002)
         drd.compile(optimizer=optimizer, loss=[loss])
         drdp.compile(optimizer=optimizer, loss=[loss])
         drdp.summary()
-        drd.summary()
-        # plot_model(drd)
+        # drd.summary()
+        # plot_model(drd,show_shapes=True)
         model_dict[layer_name] = drd
         prob_model_dict[layer_name] = drdp
 
     # plot_model(Model([wv1,wv2],model_outs,name="Deep_Relationship_Discovery"),show_shapes=True,to_file="DRD.png")
     # model_dict["common"]=common_layers_model
     common_layers_model.summary()
-    return model_dict,prob_model_dict
+    return model_dict, prob_model_dict
 
-def train_on_assertions(model,prob_model, data, epoch_amount=100, batch_size=32,save_folder = "drd"):
 
+def train_on_assertions(model, prob_model, data, epoch_amount=100, batch_size=32, save_folder="drd"):
     retroembeddings = "trained_models/retroembeddings/2019-04-0813:03:02.430691/retroembeddings.h5"
-    # retroembeddings = "retrogan/numberbatch.h5"
     retrofitted_embeddings = pd.read_hdf(retroembeddings, "mat", encoding='utf-8')
     training_data_dict = {}
     training_func_dict = {}
@@ -135,8 +135,8 @@ def train_on_assertions(model,prob_model, data, epoch_amount=100, batch_size=32,
         training_data_dict[stuff[0]].append(i)
 
     def load_batch(output_name):
-        print("Loading batch for",output_name)
-        #TODO LOAD BATCH OF A CERTAIN TYPE
+        print("Loading batch for", output_name)
+        # TODO LOAD BATCH OF A CERTAIN TYPE
         print("\nDone\n")
         l = len(training_data_dict[output_name])
         iterable = list(range(0, l))
@@ -158,12 +158,11 @@ def train_on_assertions(model,prob_model, data, epoch_amount=100, batch_size=32,
                     except:
                         continue
                 # print(np.array(x_1),np.array(x_2),np.array(y))
-                yield np.array(x_1),np.array(x_2),np.array(y)
+                yield np.array(x_1), np.array(x_2), np.array(y)
             except Exception as e:
                 print(e)
                 return False
         return True
-
 
     for epoch in range(epoch_amount):
         total_loss = 0
@@ -179,46 +178,48 @@ def train_on_assertions(model,prob_model, data, epoch_amount=100, batch_size=32,
         while True:
             for output in exclude:
                 try:
-                    x_1,x_2,y = training_func_dict[output].__next__()
+                    x_1, x_2, y = training_func_dict[output].__next__()
                     # print(x_1.shape)
                     # print(x_2.shape)
                     # print(y.shape)
-                    loss = model[output.replace("/r/", "")].train_on_batch(x={'retro_word_1':x_1,'retro_word_2':x_2},y=y)
+                    loss = model[output.replace("/r/", "")].train_on_batch(x={'retro_word_1': x_1, 'retro_word_2': x_2},
+                                                                           y=y)
                     # loss_2 = model[output.replace("/r/", "")].train_on_batch(x={'retro_word_1':x_2,'retro_word_2':x_1},y=y)
-                    total_loss+=loss
-                    iter+=1
-                    if loss>10:
-                        print("Loss",output,loss)
+                    total_loss += loss
+                    iter += 1
+                    if loss > 10:
+                        print("Loss", output, loss)
                 except Exception as e:
-                    print("Error in",output,str(e))
+                    print("Error in", output, str(e))
                     if 'the label' not in str(e):
                         tasks_completed[output] = True
             if False not in tasks_completed.values() or \
-                    len([x for x in tasks_completed.values() if x])/len(tasks_completed.values())>0.6:
+                    len([x for x in tasks_completed.values() if x]) / len(tasks_completed.values()) > 0.6:
                 for output in exclude:
                     training_func_dict[output] = load_batch(output)
                 break
-        print("Avg loss",total_loss/iter)
-        print(str(epoch)+"/"+str(epoch_amount))
+        print("Avg loss", total_loss / iter)
+        print(str(epoch) + "/" + str(epoch_amount))
         print("Saving...")
         try:
             os.mkdir(save_folder)
         except Exception as e:
             print(e)
         for key in model.keys():
-            model[key].save(save_folder+"/"+key+".model")
+            model[key].save(save_folder + "/" + key + ".model")
         for key in prob_model.keys():
             prob_model[key].save(save_folder + "/" + key + "probability.model")
             # exit()
         print("Testing")
         model_name = "PartOf"
         test_model(model, model_name=model_name)
-        test_model(prob_model,model_name=model_name)
+        test_model(prob_model, model_name=model_name)
+
 
 def create_data(use_cache=True):
     if os.path.exists("tmp/valid_rels.hd5") and use_cache:
         print("Using cache")
-        return pd.read_hdf("tmp/valid_rels.hd5","mat")
+        return pd.read_hdf("tmp/valid_rels.hd5", "mat")
 
     assertionspath = "retrogan/conceptnet-assertions-5.6.0.csv"
     valid_relations = []
@@ -236,10 +237,10 @@ def create_data(use_cache=True):
                 weight = info["weight"]
                 c1_split = assertion_row[2].split("/")
                 # print(c1_split)
-                c1 = "/c/en/"+c1_split[3]
+                c1 = "/c/en/" + c1_split[3]
                 c2_split = assertion_row[3].split("/")
-                c2 = "/c/en/"+c2_split[3]
-                valid_relations.append([assertion_row[1], c1,c2, weight])
+                c2 = "/c/en/" + c2_split[3]
+                valid_relations.append([assertion_row[1], c1, c2, weight])
             except Exception as e:
                 print(e)
                 # pass
@@ -256,22 +257,22 @@ def load_data(path):
     return data
 
 
-def test_model(model_dict,model_name="all",normalizers=None):
+def test_model(model_dict, model_name="all", normalizers=None):
     print("testing")
-    global w1,w2
-    res = model_dict[model_name].predict(x={"retro_word_1":w1,
-                                     "retro_word_2":w2})
+    global w1, w2
+    res = model_dict[model_name].predict(x={"retro_word_1": w1,
+                                            "retro_word_2": w2})
     print(res)
     if normalizers is not None:
         norm_res = normalizers[model_name].transform(res)
         print(norm_res)
 
 
-def normalize_outputs(model,save_folder="./drd",use_cache=True):
-    save_path = save_folder+"/"+"normalization_dict.pickle"
+def normalize_outputs(model, save_folder="./drd", use_cache=True):
+    save_path = save_folder + "/" + "normalization_dict.pickle"
     if use_cache:
         try:
-            norm_dict = pickle.load(open(save_path,'rb'))
+            norm_dict = pickle.load(open(save_path, 'rb'))
             return norm_dict
         except Exception as e:
             print(e)
@@ -285,27 +286,29 @@ def normalize_outputs(model,save_folder="./drd",use_cache=True):
 
     # Load our data
     retroembeddings = "trained_models/retroembeddings/2019-04-0813:03:02.430691/retroembeddings.h5"
-    retrofitted_embeddings = pd.read_hdf(retroembeddings,"mat")
-    w1 = np.array(retrofitted_embeddings.loc[standardized_concept_uri("en","iphone")]).reshape(1,300)
+    retrofitted_embeddings = pd.read_hdf(retroembeddings, "mat")
+    w1 = np.array(retrofitted_embeddings.loc[standardized_concept_uri("en", "iphone")]).reshape(1, 300)
     for i in range(len(retrofitted_embeddings.index)):
         x1.append(w1)
-        x2.append(np.array(retrofitted_embeddings.iloc[i]).reshape(1,300))
+        x2.append(np.array(retrofitted_embeddings.iloc[i]).reshape(1, 300))
     for rel in relations:
-        rel_normer = sklearn.preprocessing.MinMaxScaler().fit(model[rel.replace("/r/", "")].predict(x={"retro_word_1":np.array(x1).reshape(len(x1),300),
-                                     "retro_word_2":np.array(x2).reshape(len(x1),300)}))
+        rel_normer = sklearn.preprocessing.MinMaxScaler().fit(
+            model[rel.replace("/r/", "")].predict(x={"retro_word_1": np.array(x1).reshape(len(x1), 300),
+                                                     "retro_word_2": np.array(x2).reshape(len(x1), 300)}))
         normalization_dict[rel.replace("/r/", "")] = rel_normer
         print(rel_normer)
-    pickle.dump(normalization_dict,open(save_path,"wb"))
+    pickle.dump(normalization_dict, open(save_path, "wb"))
     return normalization_dict
 
-def load_model_ours(save_folder = "./drd",model_name="all"):
+
+def load_model_ours(save_folder="./drd", model_name="all"):
     model_dict = {}
     if model_name == 'all':
         for rel in relations:
-            print("Loading",rel)
+            print("Loading", rel)
             layer_name = rel.replace("/r/", "")
-            model_dict[layer_name] = load_model(save_folder+"/"+layer_name+".model",
-                                                custom_objects={"ConstMultiplierLayer":ConstMultiplierLayer})
+            model_dict[layer_name] = load_model(save_folder + "/" + layer_name + ".model",
+                                                custom_objects={"ConstMultiplierLayer": ConstMultiplierLayer})
 
     else:
         layer_name = model_name.replace("/r/", "")
@@ -320,30 +323,28 @@ def load_model_ours(save_folder = "./drd",model_name="all"):
     return model_dict
 
 
-
-
 if __name__ == '__main__':
     # save_folder =     "./trained_models/deepreldis/"+str(datetime.datetime.now())
     retroembeddings = "trained_models/retroembeddings/2019-04-0813:03:02.430691/retroembeddings.h5"
     retrofitted_embeddings = pd.read_hdf(retroembeddings, "mat")
-    global w1,w2
-    w1 = np.array(retrofitted_embeddings.loc[standardized_concept_uri("en","phone")]).reshape(1,300)
-    w2 = np.array(retrofitted_embeddings.loc[standardized_concept_uri("en","app")]).reshape(1,300)
+    global w1, w2
+    w1 = np.array(retrofitted_embeddings.loc[standardized_concept_uri("en", "phone")]).reshape(1, 300)
+    w2 = np.array(retrofitted_embeddings.loc[standardized_concept_uri("en", "app")]).reshape(1, 300)
     model_name = "UsedFor"
     del retrofitted_embeddings
     gc.collect()
     print("Creating model...")
-    model,prob_model = create_model()
+    model, prob_model = create_model()
     print("Done\nLoading data")
     # model = load_model_ours()
     data = create_data(use_cache=True)
     # # data = load_data("valid_rels.hd5")
     print("Done\nTraining")
-    train_on_assertions(model,prob_model, data)
+    train_on_assertions(model, prob_model, data)
     print("Done\n")
     # model = load_model_ours(save_folder="trained_models/deepreldis/2019-04-24_1_sigmoid",model_name=model_name)
     # model = load_model_ours(save_folder="trained_models/deepreldis/2019-04-24_1_sigmoid",model_name="all")
     # normalizers = normalize_outputs(model,save_folder="trained_models/deepreldis/2019-04-1614:43:00.000000")
     # normalizers = normalize_outputs(model,use_cache=False)
-    test_model(model,normalizers=None,model_name=model_name)
+    test_model(model, normalizers=None, model_name=model_name)
     # Output needs to be the relationship weights
